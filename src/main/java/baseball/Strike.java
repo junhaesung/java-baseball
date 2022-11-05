116de47a5030365302fec35374fa234f83be6afa
Be-student <presentsong@naver.com>
2022-11-05 11:59:40 +0900

    feat: strike와 ball의 개념 추가

---
+++ src/main/java/baseball/Strike.java
@@ -0,0 +1,20 @@
+package baseball;
+
+import java.util.Arrays;
+
+public enum Strike {
+    ZERO(0), ONE(1), TWO(2), THREE(3);
+
+    private final int value;
+
+    Strike(int value) {
+        this.value = value;
+    }
+
+    public static Strike from(Integer value) {
+        return Arrays.stream(Strike.values())
+                .filter(it -> it.value == value)
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+}
